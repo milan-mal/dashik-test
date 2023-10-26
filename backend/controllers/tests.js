60a0d1847efa71d4ce78d9a5bd81d2f4ff1e3087
Milan M. <milanmalenak@gmail.com>
2023-10-26 21:53:58 +0200

    hotfix error handling getting a single test

--- backend/controllers/tests.js
+++ backend/controllers/tests.js
@@ -8,13 +8,21 @@ testRouter.get('/', async (req, res) => {
   res.status(200).json(tests)
 })
 
-testRouter.get('/:id', async (req, res) => {
+testRouter.get('/:id', async (req, res) =>  {
   const test = await Test.findById(req.params.id)
-  if( test ){
-    res.json(test)
-  } else {
-    res.status(404).end()
-  }
+    .then(() => res.json(test))
+    .catch((error) => { // TODO: clean up error handling
+      if ( error.name === 'CastError' ){
+        return res.status(400).send({ error: 'malformatted id' })
+      } else {
+        logger.error(error.message)
+      }
+    })
+  // if( test ){
+  //   res.json(test)
+  // } else {
+  //   res.status(404).end()
+  // }
 })
 
 export default testRouter
